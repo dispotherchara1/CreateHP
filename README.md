72fcd4eef77cb07b265c7295965688c7b843383c
Rikiya Yamamoto <nkc.yamamoto.rikiya@gmail.com>
2020-11-04 13:33:35 +0900

    特になし

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # CreateHP
 ホムペつくるお
+
+
+HTML 使用必須
+
+CSS  場合によって使用 ってか大半使用でしょ
+
+PHP  もしかしたら
+
+JavaScript  あやしい
--- 使いまし/1/Rubby/bbs.rb
+++ 使いまし/1/Rubby/bbs.rb
@@ -6,9 +6,9 @@ f = open("bbs.dat","r")
 s = CGI.escapeHTML(f.read)
 f.close
 
-print "Content-type: text/html\n\n"
+p "Content-type: text/html\n\n"
 
-print <<EOF
+p <<EOF
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN"
     "ttp://www.w3.org/TR/html4/strict.dtd">
 <html lang="ja">
